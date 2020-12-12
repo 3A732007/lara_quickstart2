6c7887eb71366632bd14f07b873391dbb1cb1600
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-12-12 18:06:34 +0800

    user關聯(取得擁有此任務的使用者)

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,4 +14,12 @@ class Task extends Model
      * @var array
      */
     protected $fillable = ['name'];
+    
+    /**
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
